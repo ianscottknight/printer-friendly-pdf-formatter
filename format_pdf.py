3917dcdd73502bd272ea5d75f748994fc1a155bd
Ian Scott Knight <isk@alumni.stanford.edu>
2021-04-22 17:29:42 -0500

    adds output directory filepath argument; reconfigures arguments; adds stripping of whitespace in filepath arguments

--- format_pdf.py
+++ format_pdf.py
@@ -8,17 +8,22 @@ from PyPDF2 import PdfFileWriter, PdfFileReader
 # parse filepath argument
 
 parser = argparse.ArgumentParser()
-parser.add_argument("pdf_filepath", type=pathlib.Path)
+parser.add_argument("--input_pdf_filepath", "-i", type=pathlib.Path)
+parser.add_argument("--output_directory_filepath", "-o", type=pathlib.Path)
 args = parser.parse_args()
 
-pdf_filepath = str(args.pdf_filepath)
+input_pdf_filepath = str(args.input_pdf_filepath)
+output_directory_filepath = str(args.output_directory_filepath)
 
+# strip input and output straings
+input_pdf_filepath = input_pdf_filepath.strip()
+output_directory_filepath = output_directory_filepath.strip()
 
 # get output filepaths
 
 QUOTE_CHARS = ["'", '"']
 
-dirpath, filename = os.path.split(pdf_filepath)
+dirpath, filename = os.path.split(input_pdf_filepath)
 
 for quote_char in QUOTE_CHARS:
     if filename[-1] == quote_char:
@@ -29,14 +34,14 @@ name, ext = os.path.splitext(filename)
 pdf_filename_front = f"print_this_first" + ext
 pdf_filename_back = f"print_this_second" + ext
 
-pdf_filepath_front = os.path.join(os.getcwd(), pdf_filename_front)
-pdf_filepath_back = os.path.join(os.getcwd(), pdf_filename_back)
+output_pdf_filepath_front = os.path.join(output_directory_filepath, pdf_filename_front)
+output_pdf_filepath_back = os.path.join(output_directory_filepath, pdf_filename_back)
 
 
 # Step 1: put 2 pages per sheet of paper
 
 temp_1_filepath = os.path.join(os.getcwd(), "temp_1.pdf")
-os.system(f"pdfautonup {pdf_filepath} --size a3 --output {temp_1_filepath}")
+os.system(f"pdfautonup {input_pdf_filepath} --size a3 --output {temp_1_filepath}")
 
 
 # Step 2: split pages into 2 different PDFs (one with front pages, one with back pages)
@@ -58,7 +63,7 @@ with open(temp_1_filepath, "rb") as f:
     if num_pages % 2 == 1:
         pdf_back.addBlankPage()
 
-    with open(pdf_filepath_front, "wb") as g:
+    with open(output_pdf_filepath_front, "wb") as g:
         pdf_front.write(g)
 
     with open(temp_2_filepath, "wb") as g:
@@ -75,7 +80,7 @@ with open(temp_2_filepath, "rb") as f:
     for page in range(num_pages - 1, -1, -1):
         pdf_back.addPage(source_pdf.getPage(page))
 
-    with open(pdf_filepath_back, "wb") as g:
+    with open(output_pdf_filepath_back, "wb") as g:
         pdf_back.write(g)
 
 
